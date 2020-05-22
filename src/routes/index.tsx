b3e2b6e1e46aa07ea506ec16753178f88e739860
Beatriz Murase <biamurase@gmail.com>
2020-05-22 20:22:01 -0300

    feat: forgot password page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,10 +7,12 @@ import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 
 import Dashboard from '../pages/Dashboard';
+import ForgotPassword from '../pages/ForgotPassword';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
+    <Route path="/forgot" exact component={ForgotPassword} />
     <Route path="/signup" component={SignUp} />
     <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
